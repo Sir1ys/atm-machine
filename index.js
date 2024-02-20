ff14cd7c9b0c1aca52cc5337489ed38fa1269ae9
Sir1ys <sashakoroviy@gmail.com>
2024-02-20 22:12:07 +0000

    test for complex amounts is passed

--- index.js
+++ index.js
@@ -10,8 +10,38 @@ const contentOfAtm = [
   { value: 1, type: "coin" },
 ];
 
+function recursion(amount) {
+  const element = contentOfAtm.find((e) => amount >= e.value);
+
+  if (amount === 0) return 0;
+
+  return JSON.stringify(element) + " " + recursion(amount - element.value);
+}
+
 function makeAWithdraw(amount) {
-  return `Your withdraw is ${amount} coin of 1`;
+  const resultOfRecursion = recursion(amount).split(" ");
+  let string = "Your withdraw is";
+
+  resultOfRecursion.forEach((e, i) => {
+    const lastObjectIndex = resultOfRecursion.length - 2;
+    const { value, type } = JSON.parse(e);
+
+    switch (true) {
+      case i === 0:
+        string += ` 1 ${type} of ${value}`;
+        break;
+      case i > 0 && i < lastObjectIndex:
+        string += `, 1 ${type} of ${value}`;
+        break;
+      case i === lastObjectIndex:
+        string += ` and 1 ${type} of ${value}`;
+        break;
+      default:
+        break;
+    }
+  });
+
+  return string;
 }
 
 module.exports = makeAWithdraw;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,22 @@
 const makeAWithdraw = require("./index");
 
-test("write correct output when you want to withdraw 1 coin", () => {
+test("return correct output when you want to withdraw simple amount", () => {
   expect(makeAWithdraw(1)).toBe("Your withdraw is 1 coin of 1");
+  expect(makeAWithdraw(2)).toBe("Your withdraw is 1 coin of 2");
+  expect(makeAWithdraw(5)).toBe("Your withdraw is 1 bill of 5");
+});
+
+test("return correct output when you want to withdraw complex amount", () => {
+  expect(makeAWithdraw(8)).toBe(
+    "Your withdraw is 1 bill of 5, 1 coin of 2 and 1 coin of 1"
+  );
+  expect(makeAWithdraw(11)).toBe(
+    "Your withdraw is 1 bill of 10 and 1 coin of 1"
+  );
+  expect(makeAWithdraw(13)).toBe(
+    "Your withdraw is 1 bill of 10, 1 coin of 2 and 1 coin of 1"
+  );
+  expect(makeAWithdraw(52)).toBe(
+    "Your withdraw is 1 bill of 50 and 1 coin of 2"
+  );
 });
